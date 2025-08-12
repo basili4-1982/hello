ac5ce6889e5b9a961d19d0c970f23f0349660ac3
Vasiliy Ahmedzhanov <basili4.1982@gmail.com>
2025-08-12 04:33:38 +0300

    ci4 - run linters

--- main.go
+++ main.go
@@ -18,12 +18,17 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if err := json.NewEncoder(w).Encode(resp); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+
+	var a = "sss"
+
+	a = a
 }
 
 func main() {
 	http.HandleFunc("/", handler)
 	log.Println("Server started on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
+
 		log.Fatal(err)
 	}
 }
